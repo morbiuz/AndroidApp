ba595a7ee63374514eba3af2ad36af7ae7bca77d
morbiuz <morbiuz@gmail.com>
2014-05-09 19:59:49 +0200

    Cambiados nombres de objetos y añadido listener

--- Nuevo Proyecto/src/main/java/com/app/nuevoproyecto/nuevoproyecto/MainActivity.java
+++ Nuevo Proyecto/src/main/java/com/app/nuevoproyecto/nuevoproyecto/MainActivity.java
@@ -4,14 +4,35 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    TextView mainTextView;
+    Button mainButton;
+    EditText mainEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 1. Access the TextView defined in layout XML
+        // and then set its text
+        mainTextView = (TextView) findViewById(R.id.main_textview);
+        mainTextView.setText("Set in Java!");
+
+        // 2. Access the Button defined in layout XML
+        // and listen for it here
+        mainButton = (Button) findViewById(R.id.main_button);
+        mainButton.setOnClickListener(this);
+
+        // 3. Access the EditText defined in layout XML
+        mainEditText = (EditText) findViewById(R.id.main_edittext);
     }
 
 
@@ -33,4 +54,11 @@ public class MainActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onClick(View view) {
+        // Test the Button
+        mainTextView.setText("Hola " + mainEditText.getText().toString() + "!");
+    }
 }
+
