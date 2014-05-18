402fc6eb940b9d54d8c06218436057dcdc6820a8
morbiuz <morbiuz@gmail.com>
2014-05-18 20:13:29 +0200

    Defined specific stop() method so the song can be played again from the start if play is pressed afterwards.

--- Nuevo Proyecto/src/main/java/com/app/nuevoproyecto/nuevoproyecto/MainActivity.java
+++ Nuevo Proyecto/src/main/java/com/app/nuevoproyecto/nuevoproyecto/MainActivity.java
@@ -1,12 +1,8 @@
 package com.app.nuevoproyecto.nuevoproyecto;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
-// import android.app.FragmentManager;
 import android.media.MediaPlayer;
 import android.support.v4.app.FragmentManager;
-import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -77,10 +73,7 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -109,7 +102,7 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         }
 
         else if(view == stopButton) {
-            mp.stop();
+            stop();
         }
     }
 
@@ -122,6 +115,18 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         }
     }
 
+    private void stop() {
+
+        mp.stop();
+
+        try {
+            mp.prepare();
+            mp.seekTo(0);
+        }
+        catch (Throwable t) {
+            showError(t);
+        }
+    }
     private void showError(Throwable t) {
         AlertDialog.Builder builder=new AlertDialog.Builder(this);
 
